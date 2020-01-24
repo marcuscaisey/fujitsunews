0741c5c923ce4f3a7e4bb297a1fe47afc690e4ac
Marcus Caisey <marcus.caisey@uk.fujitsu.com>
2020-01-24 11:15:10 +0000

    Serve with waitress

--- formserver.py
+++ formserver.py
@@ -1,6 +1,7 @@
 import datetime
 
 import flask
+import waitress
 
 app = flask.Flask(__name__)
 
@@ -9,17 +10,15 @@ app = flask.Flask(__name__)
 def contact():
     form = flask.request.form
     header = f"Form submission received at {datetime.datetime.now():%H:%M:%S}"
-    output = f"""
-{header}
+    output = f"""{header}
 {len(header) * '-'}
 name: {form['name']}
 email: {form['email']}
 message: {form['message']}
-
-    """
+"""
     print(output)
     return "", 200
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", port=80)
+    waitress.serve(app, host="0.0.0.0", port=80)
